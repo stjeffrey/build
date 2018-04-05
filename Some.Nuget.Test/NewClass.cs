786cb578606e9a4e7c3bd89caf8763b93ed3175c
Stephane Jeffrey <jeffreys@flextechservices.com>
2018-04-05 13:54:56 -0400

    New class

---
+++ Some.Nuget.Test/NewClass.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="NewClass.cs" company="Flex Tech Services Inc.">
+//   Copyright (c) 2017, Flex Tech Services Inc.
+// </copyright>
+
+namespace Some.Nuget.Test
+{
+    public class NewClass
+    {
+        
+    }
+}
